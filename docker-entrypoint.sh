b74b2d35a0df8200121a186b8a0e08034d0c58c7
Michael Baynton <mike@mbaynton.com>
2016-10-24 20:56:46 -0500

    Add code to set XDEBUG_CONFIG to point back to the docker host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,14 @@
 /usr/sbin/service vsftpd start
 
 CONTAINER_CMD="/curator/prophusion-tests.sh"
+
+# If xdebug connect back to the docker host is requested, set environment
+if [ "$DOCKER_REMOTE_XDEBUG" == "1" ]
+then
+  HOST_IP=$(/sbin/ip -4 route list 0/0 | /usr/bin/awk '/default/ { print $3 }')
+  XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_host=$HOST_IP remote_port=9000"
+  # echo $XDEBUG_CONFIG
+  export XDEBUG_CONFIG
+fi
+
 . /prophusion-base-entrypoint.sh
