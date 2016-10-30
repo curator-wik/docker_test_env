d647ca288dfacbc2ada782b39f245ebbd06e70f4
Mike Baynton <mike@mbaynton.com>
2016-10-29 21:55:48 -0500

    Provide path mapping cue to phpstorm via PHP_IDE_CONFIG. Comment out attempts to run phpstorm-customized phpunit from within phpstorm for now.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,12 +11,13 @@ then
   XDEBUG_CONFIG="remote_enable=1 remote_mode=req remote_host=$HOST_IP remote_port=9000"
   # echo $XDEBUG_CONFIG
   export XDEBUG_CONFIG
+  export PHP_IDE_CONFIG="serverName=docker-test-env.curatorwik"
 
-  IDE_PHPUNIT_CUSTOM_LOADER="$PHPUNIT"
-  export IDE_PHPUNIT_CUSTOM_LOADER
-  cp /host_tmp/ide-phpunit.php /tmp
-  PHPUNIT="php /tmp/ide-phpunit.php"
-  export PHPUNIT
+#  IDE_PHPUNIT_CUSTOM_LOADER="$PHPUNIT"
+#  export IDE_PHPUNIT_CUSTOM_LOADER
+#  cp /host_tmp/ide-phpunit.php /tmp
+#  PHPUNIT="php /tmp/ide-phpunit.php"
+#  export PHPUNIT
 fi
 
 . /prophusion-base-entrypoint.sh
