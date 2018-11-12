432f1f3f92a20802900ae06ecb6fe27403c1ac07
Mike Baynton <mike@mbaynton.com>
2018-11-11 20:28:54 -0600

    Changes to support running tests as non-root user

--- container-setup.sh
+++ container-setup.sh
@@ -2,9 +2,12 @@
 # Extract everything.
 cd /
 umask 0022
-tar --no-same-owner --no-same-permissions -xf files.tar
+tar -xf files.tar
 
 ln -s /usr/local/bin/phpunit-5.5.4.phar /usr/local/bin/phpunit-5.5
+
+# The www-data user was arbitrarily chosen to run the tests; make it more amenable to usage.
+usermod -d /home/www-data -s /bin/bash www-data
 
 adduser --disabled-password --gecos 'FTP Test' ftptest
 echo 'ftptest:Asdf1234' | chpasswd
@@ -13,6 +16,8 @@ mkdir /home/ftptest/www && chown ftptest:ftptest /home/ftptest/www && chmod 0755
 adduser --disabled-password --gecos 'FTP Test Chroot' ftptest_chroot
 echo 'ftptest_chroot:Asdf1234' | chpasswd
 mkdir /home/ftptest_chroot/www && chown ftptest_chroot:ftptest_chroot /home/ftptest_chroot/www && chmod 0755 /home/ftptest_chroot/www
+
+
 
 # Make the user's home directory owned by root, so it is not writable by ftptest and vsftpd will chroot it
 chown root:root /home/ftptest_chroot
